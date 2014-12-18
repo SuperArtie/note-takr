b213b253c630927a946a9fdc32c14ae1457921fc
Nathan Vasquez <nathan.superartie@gmail.com>
2014-12-18 14:49:35 -0600

    seventh commit

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,19 +1,23 @@
-var config = {};
-config.seleniumAddress = 'http://localhost:4444/wd/hub';
-config.multiCapabilities = [
-    {
-        'browserName': 'chrome'
-    }
-];
+var config;
+
+if(process.env.TRAVIS_JOB_NUMBER){
+    config = require('./protractor-sauce.conf');
+}else{
+    config = require('./protractor-local.conf');
+}
+
 config.suites = {
     homepage: 'test/e2e/homepage/**/*.spec.js',
-    authentication: 'test/e2e/authentication/**/*.spec.js'
+    authentication: 'test/e2e/authentication/**/*.spec.js',
+    notes: 'test/e2e/notes/**/*.spec.js'
 };
+
 config.jasmineNodeOpts = {
-    isVerbose: false,
+    isVerbose: true,
     showColors: true,
     defaultTimeoutInterval: 30000
 };
 
 config.baseUrl = 'http://localhost:9001';
+
 exports.config = config;
